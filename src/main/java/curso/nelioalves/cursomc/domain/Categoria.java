78564044211d29dfc2f40dcafb469a6af21b9dec
barbosaap <andrebarbosapereira@gmail.com>
2019-06-25 13:33:47 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/curso/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/curso/nelioalves/cursomc/domain/Categoria.java
@@ -1,5 +1,11 @@
 package curso.nelioalves.cursomc.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +17,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Categoria {
+
+	@Id
+	@GeneratedValue(
+		strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cat")
+	@EqualsAndHashCode.Include
 	private Integer id;
+	
+	@Column(name = "nm_cat", length = 150)
 	private String nome;
 }
