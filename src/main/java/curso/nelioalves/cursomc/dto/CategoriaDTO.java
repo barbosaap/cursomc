f0a725048a7d6e2967ee5e5a1bc01d3a3e9f1850
barbosaap <andrebarbosapereira@gmail.com>
2019-06-27 13:33:40 -0300

    Validação sintática com Bean Validation

--- src/main/java/curso/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/curso/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -1,5 +1,9 @@
 package curso.nelioalves.cursomc.dto;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import curso.nelioalves.cursomc.domain.Categoria;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,6 +16,8 @@ public class CategoriaDTO {
 
 	private Integer id;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO(Categoria cat) {
--- src/main/java/curso/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/curso/nelioalves/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import curso.nelioalves.cursomc.domain.Categoria;
+import curso.nelioalves.cursomc.dto.CategoriaDTO;
 import curso.nelioalves.cursomc.repositories.CategoriaRepository;
 import curso.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import curso.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage,
 				Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
