03fbc1ace4576bf8312d3e9b75e97674b54f67b7
barbosaap <andrebarbosapereira@gmail.com>
2019-06-27 17:28:14 -0300

    Validação customizada: CPF ou CNPJ na inserção de Cliente

--- src/main/java/curso/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/curso/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -1,27 +1,52 @@
 package curso.nelioalves.cursomc.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import curso.nelioalves.cursomc.services.validation.ClienteInsert;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ClienteInsert
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClienteNewDTO {
-
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
+	
 	private String telefone3;
 	
 	private Integer cidadeId;
